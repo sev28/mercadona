ad383f40a8ab4bd86bedbb78ebaa28d2e629e716
sev28 <charlisev@hotmail.fr>
2023-11-14 10:29:55 +0100

    modifie PorductController

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -16,7 +16,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/products')]
 class ProductsController extends AbstractController
 {
-    #[Route('/', name: 'app_products_index', methods: ['GET'])]
+    #[Route('/', name: 'app_products_index', methods: ['PUT'])]
     public function index(ProductsRepository $productsRepository, Request $request): Response
     {
         $data = new SearchData();
