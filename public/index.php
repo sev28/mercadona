a452e3210c7bcf9a068f62718a208855ebef313d
sev28 <charlisev@hotmail.fr>
2023-11-14 10:19:14 +0100

    modifie Procfile

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\BrowserKit\Request;
+use Symfony\Component\BrowserKit\Response;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+
+  
