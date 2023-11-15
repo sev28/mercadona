da056d91dc2cb66593ec37e4318e178b9808da01
sev28 <charlisev@hotmail.fr>
2023-11-15 23:32:47 +0100

    Coverage report

--- tests/Controller/CategoryCrudControllerTest.php
+++ tests/Controller/CategoryCrudControllerTest.php
@@ -4,6 +4,8 @@ namespace App\tests\Controller;
 
 use App\Entity\Admin;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryCrudControllerTest extends WebTestCase
 {
@@ -14,12 +16,26 @@ class CategoryCrudControllerTest extends WebTestCase
        $urlGenerator = $client->getContainer()->get('router');
 
        // Recup entity manager
-       $entityManager = $client->getContainer()->get('doctrine:orm.entity_manager');
+       $entityManager = $client->getContainer()->get('doctrine.orm.entity_manager');
 
-       $user = $entityManager->find(Admin::class, 1);
+       $admin = $entityManager->find(Admin::class, 2);
+       $client->loginUser($admin);
+
        // Se rendre sur la page de création d'une catégorie
+       $crawler = $client->request(Request::METHOD_GET, $urlGenerator->generate('category.new'));
+       
        // gérer le formulaire
+       $form = $crawler->filter('form[name=caterory]')->form([
+            'category[wording]' => 'Un libéllé',
+       ]);
+
+       $client->submit($form);
+
        // Gérer la redirection
+       $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+       $client->followRedirect();
        // la route 
+       $this->assertRouteSame('category.index');
     }
 }
