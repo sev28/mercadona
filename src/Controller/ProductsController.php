1201a759939e0849662a7b221b84ca9238666068
sev28 <charlisev@hotmail.fr>
2023-12-03 15:52:58 +0100

    change

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -15,10 +15,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-#[Route('/products', methods: ['GET'])]
+#[Route('/products')]
 class ProductsController extends AbstractController
 {
-    #[Route('/', name: 'app_products_index', methods: ['GET', 'HEAD'])]
+    #[Route('/', name: 'app_products_index', methods: ['GET'])]
     public function index(ProductsRepository $repository, Request $request, PaginatorInterface $paginator, ProductsRepository $productsRepository): Response
     {
         $data = new SearchData();
